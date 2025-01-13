5cc35efce52708d10a26c400c6d0b604fab68421
loisblin <blinlois@gmail.com>
2025-01-13 14:29:59 +0200

    ml

--- machine-learning/ml.py
+++ machine-learning/ml.py
@@ -10,21 +10,27 @@ client = pymongo.MongoClient(uri)
 db = client['bitcoin-data']
 collection = db['bitcoin-batch']
 
-
 data = collection.find()
 df = pd.DataFrame(list(data))
 
-df = df[['open', 'high', 'low', 'variation_percentage', 'close']]  
+
+df = df[['close']]  
+df.dropna(inplace=True)
 
 
-df.dropna(inplace=True)  
+n_lags = 7  
+for i in range(1, n_lags + 1):
+    df[f'lag_{i}'] = df['close'].shift(i)
 
 
-X = df[['open', 'high', 'low']] 
+df.dropna(inplace=True)
+
+
+X = df[[f'lag_{i}' for i in range(1, n_lags + 1)]]
 y = df['close']  
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=False)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
@@ -32,14 +38,11 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-
-mae = mean_absolute_error(y_test, y_pred)  
+mae = mean_absolute_error(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 
 print(f"Mean Absolute Error (MAE): {mae}")
+print(f"Mean Squared Error (MSE): {mse}")
 
-
-
-for i in range(10): 
-    print(f"real : {y_test.iloc[i]}, predict : {y_pred[i]}")
-
+for i in range(10):
+    print(f"Real: {y_test.iloc[i]}, Predicted: {y_pred[i]}")
